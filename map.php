fb2b29dc4b79182ef13a070f600c45cd4a4270d7
gillete98 <julian.loaiza@gmail.com>
2017-02-14 12:04:49 -0800

    Small Cells

--- map.php
+++ map.php
@@ -148,6 +148,18 @@ $aws = "10.2.4.212"//"ec2-54-69-137-192.us-west-2.compute.amazonaws.com"
                         </ul>
 					</li>                     
 
+                    
+					<li class="has-sub">
+						<a href="#">
+						    <b class="caret pull-right"></b>
+						    <span>Small Cells</span>
+					    </a>
+						<ul class="sub-menu">
+						    <li><a href="#"><input type="checkbox" name="checkMaps8" id="check8" value="" onclick="tiledLayer('pci','http://<?php echo $aws; ?>:8080/geoserver/gwc/service/gmaps?layers=SmallCells:smallcells_xy&zoom={z}&x={x}&y={y}&format=image/png8',0,1);"  > Cells Planned</a></li> 
+                            <li><a href="#"><input type="radio" name="optionsMaps" id="radio24" value="option24" onclick="tiledLayer('pci','http://<?php echo $aws; ?>:8080/geoserver/gwc/service/gmaps?layers=SmallCells:SmallCells_xy_density&zoom={z}&x={x}&y={y}&format=image/png8',0,1);"> Starling Density</a></li>
+                        </ul>
+					</li>
+                    
 
                     
 					<li class="has-sub">
@@ -352,7 +364,7 @@ $aws = "10.2.4.212"//"ec2-54-69-137-192.us-west-2.compute.amazonaws.com"
 						    <li><a href="#"><input type="radio" name="options3G" id="Par_3" value="LAC"> 3G-LAC</a></li>
 						    <li><a href="#"><input type="radio" name="options3G" id="Par_7" value="bsTxPwrMax1x00"> 2G-bsTxPwrMax1x00</a></li>
 
-						    <li><a href="#"><input type="radio" name="options3G" id="Par_6" value="SC"> 2G-BCCH/SC/PCI</a>  <input type="text" style="width:50px; position: relative; left:80px;" class="form-control" id="channel" placeholder="Val"></li>                            
+						    <li><a href="#"><input type="radio" name="options3G" id="Par_6" value="SC"> 2G-BCCH/SC/PCI</a>  <input type="text" style="width:80px; position: relative; left:80px;" class="form-control" id="channel" placeholder="Val"></li>                            
 						    <li><a href="#"><input type="radio" name="options3G" id="Par_10" value="NBRs"> 2G-Neighbors</a> </br> <input type="text" style="width:150px; position: relative; left:10px;" class="form-control" id="relation" placeholder="NBR"></li>                            
 
                             
@@ -381,7 +393,7 @@ $aws = "10.2.4.212"//"ec2-54-69-137-192.us-west-2.compute.amazonaws.com"
                             <div class="col-xs-14">
                             
                                 <div>
-                                    <button class="btn btn-default" onclick="clearSelection();" id="delete-button">Delete</button>
+                                    <button class="btn btn-default" onclick="clearSelection();" id="delete-button">Clear</button>
                                 </div>                        
                                                 
                                
